bc6f798c59847bf086368fb53f7cc288d7aa12ea
Andres Diaz <ivanandresdilox@gmail.com>
2021-06-29 11:35:43 -0500

    segundo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import getData from '../utils/getData';
+import React, {useEffect, useState} from 'react';
 
 const Home = () => {
-  const data = getData();
+  const [dataLista, setDataLista] = useState([])
+  const getData = async () => {
+    const url = 'https://rickandmortyapi.com/api/character/';
+    const response = await fetch(url);
+    const data = await response.json();
+    setDataLista(data.results);
+  };
+  useEffect(() => {
+    getData()
+  }, [])
+  
   return (
-    <div />
+    <div>
+      <h1>Aqui deberia aparace  los p </h1>
+      {dataLista.map((character) => <p key={character.id}>{character.name}</p>)}
+    </div>
   );
 };
 
